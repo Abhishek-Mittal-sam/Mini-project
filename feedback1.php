ce2f85178e80a7e1d669778bd0dd4bcc12b180b0
ABHISHEK MITTAL <81106129+Abhishek-Mittal-sam@users.noreply.github.com>
2022-01-15 11:50:12 +0530

    Add files via upload

---
+++ feedback1.php
@@ -0,0 +1,34 @@
+<?php
+include "connection.php";
+include "navbar.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Feedback</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+  
+    <style type="text/css">
+      body
+      {	height:700px;
+	width:2800px; 
+	max-width: 100%;
+
+	background-image: url("66.jpg");
+	background-attachment: fixed;
+	background-repeat: no-repeat ;
+	background-size: cover;
+
+      }
+    </style>    
+</head>
+<body>
+    
+</body>
+</html>
